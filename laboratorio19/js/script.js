6a223bc7655d7f67154d0baafadc663c77007134
unknown <42100559@uaz.edu.mx>
2023-11-29 18:09:28 -0600

    Descripción de cambios

--- laboratorio19/js/script.js
+++ laboratorio19/js/script.js
@@ -1,50 +1,73 @@
 //valida si el NIP es correcto
-function validarNIP(){
-  //obtengo la contraseña
-  let nip = document.getElementById("nip").value;
+function validarNIP() {
+    //obtengo la contraseña
+    let nip = document.getElementById("nip").value;
 
-  //si la contraseña es correcta abro la otra ventana
-  if(nip == "1234"){
-      //document.getElementById('welcome').innerHTML = "¡Bienvenido!";
-      window.location.href = 'welcome.html';
+    //si la contraseña es correcta abro la otra ventana
+    if (nip == "1234") {
+        //document.getElementById('welcome').innerHTML = "¡Bienvenido!";
+        window.location.href = 'welcome.html';
 
 
-      setTimeout(function() {
-          // Esta parte del código se ejecutará después del retraso
-          window.location.href = 'welcome.html';
-      }, 2000); // 2000 milisegundos (2 segundos) de retraso
+        setTimeout(function () {
+            // Esta parte del código se ejecutará después del retraso
+            window.location.href = 'welcome.html';
+        }, 2000); // 2000 milisegundos (2 segundos) de retraso
 
-  }else{
-      document.getElementById('errorMensaje').innerHTML = "NIP incorrecto";
-  }
+    } else {
+        document.getElementById('errorMensaje').innerHTML = "NIP incorrecto";
+    }
 }
 
 
-//depositar cantidad
-function depositar() {
-    let cantidad = parseFloat(prompt('¿Cuanto deseas depositar?'));
-    if(!isNaN(cantidad) && cantidad >0){
-        saldo += cantidad;
-        document.getElementById('mensajeTransaccion').textContent = 'Has depositado ${cantidad}. Tu nuevo saldo es ${saldo}.';
-    } else {
-        document.getElementById('mensajeTransaccion').textContent = 'Por favor, ingrese una cantidad válida.';
-    }//fin del else
-}//fin de la funcion
+// Define la variable de saldo
+let saldo = 1000; // Saldo inicial, puedes cambiarlo según tus necesidades
 
+// Función para realizar la operación seleccionada
+function realizarOperacion() {
+    let operacion = document.getElementById("operacion").value;
+    let cantidad = parseFloat(document.getElementById("transaccion").value);
 
-//retirar cantidad
-function retirar() {
-    let cantidad = parseFloat(prompt('¿Cuanto deseas retirar?'))
-    if(!isNaN(cantidad) && cantidad >0 && cantidad <= saldo){
-        saldo -= cantidad;
-        document.getElementById('mensajeTransaccion').textContent = 'Has retirado ${cantidad}. Tu nuevo saldo es ${saldo}.';
-    } else {
-        document.getElementById('mensajeTransaccion').textContent = 'Cantidad invalida o insuficiente saldo';
-    }//fin del else
-}//fin de la funcion
+    switch (operacion) {
+        case "ver_saldo":
+            verSaldo();
+            break;
+        case "depositar":
+            if (!isNaN(cantidad) && cantidad > 0) {
+                depositar(cantidad);
+            } else {
+                document.getElementById('mensajeTransaccion').textContent = 'Por favor, ingrese una cantidad válida.';
+            }
+            break;
+        case "retirar":
+            if (!isNaN(cantidad) && cantidad > 0 && cantidad <= saldo) {
+                retirar(cantidad);
+            } else {
+                document.getElementById('mensajeTransaccion').textContent = 'Cantidad inválida o insuficiente saldo';
+            }
+            break;
+        case "salir":
+            // Agrega aquí la lógica para salir
+            break;
+        default:
+            document.getElementById('mensajeTransaccion').textContent = 'Selecciona una operación válida.';
+    }
+}
 
+// Función para depositar cantidad
+function depositar(cantidad) {
+    saldo += cantidad;
+    document.getElementById('mensajeTransaccion').textContent = `Has depositado ${cantidad}. Tu nuevo saldo es ${saldo}.`;
+}
 
-//ver saldo
+// Función para retirar cantidad
+function retirar(cantidad) {
+    saldo -= cantidad;
+    document.getElementById('mensajeTransaccion').textContent = `Has retirado ${cantidad}. Tu nuevo saldo es ${saldo}.`;
+}
+
+// Función para ver saldo
 function verSaldo() {
-    document.getElementById('mensajeTransaccion').textContent = 'Tu saldo actual es ${saldo}.';
-}//fin de la funcion 
+    document.getElementById('mensajeTransaccion').textContent = `Tu saldo actual es ${saldo}.`;
+}
+
